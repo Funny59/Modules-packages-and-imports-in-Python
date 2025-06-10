175ae6e7cb257ec310e0ef918f0c8841659fbfbb
Funny59 <artemdolgih@hotmail.com>
2025-06-10 18:55:18 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 from datetime import datetime
 import sys
-
-sys.path.append('./application')
-sys.path.append('./application/db')
-
-from salary import calculate_salary
-from people import get_employees
+from application.salary import calculate_salary
+from application.db.people import get_employees
 
 if __name__ == '__main__':
     calculate_salary()
